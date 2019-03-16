28927ca7489b2ad1e2de1a23e8688a1c5c591ff3
deventorum <deventorum@gmail.com>
2019-03-16 13:17:49 -0400

    Added sword to the game

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,33 +8,60 @@ public class Player : MonoBehaviour
 {
     public float speed;
     Animator anim;
-    public Image[] Hearts;
+    public Image[] hearts;
     public int maxHealth;
     int currentHealth;
+    public GameObject sword;
 
     void Start()
     {
         anim = GetComponent<Animator>();
-        anim.SetInteger("dir", 0);
         currentHealth = maxHealth;
-
         GetHealth();
     }
 
-    void GetHealth()
+    private void GetHealth()
     {
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            hearts[i].gameObject.SetActive(false);
+        }
         for (int i = 0; i < currentHealth; i++)
         {
-            Hearts[i].gameObject.SetActive(true);
-            Debug.Log(Hearts[i]);
+            hearts[i].gameObject.SetActive(true);
         }
     }
 
-    // Update is called once per frame
+        // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space)) 
+            Attack();
         Movement();
     }
+    private void Attack()
+    {
+        GameObject newSword = Instantiate(sword, transform.position, sword.transform.rotation);
+        int swordDir = anim.GetInteger("dir");
+        switch (swordDir)
+        {
+            case 1:
+                newSword.transform.Rotate(0, 0, 180);
+                break;
+            case 2:
+                newSword.transform.Rotate(0, 0, 0);
+                break;
+            case 3:
+                newSword.transform.Rotate(0, 0, -90);
+                break;
+            case 4:
+                newSword.transform.Rotate(0, 0, 90);
+                break;
+            default:
+                break;
+        }
+    }
+    
     private void Movement()
     {
         transform.Translate(Input.GetAxis("Horizontal") * speed * Time.deltaTime, Input.GetAxis("Vertical") * speed * Time.deltaTime, 0);
@@ -65,4 +92,5 @@ public class Player : MonoBehaviour
             anim.speed = 0;
         }
     }
+
 }
